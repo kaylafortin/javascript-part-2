b2f9d353455baee8269d42301949d1f42b39fac3
hennigk <kayla.hennig@gmail.com>
2015-09-14 20:38:47 +0000

    added function that squares a property of an object in an array

--- functions.js
+++ functions.js
@@ -45,14 +45,15 @@ Then, run some code that will sort your array by the longest name.
 Then, run some code that will sort your array by e-mail 
 address in alphabetical order.*/
 
-function Person(name, email) {
+function Person(name, email, num) {
     this.name = name;
     this.email = email;
+    this.num = num
 }
-var Kayla = new Person("Kayla", "kayla@mail.com")
-var Dave = new Person("Dave", "dave@mail.com")
-var Sir = new Person("Sir Edward", "Edward.has.long.email.com")
-var Student = new Person("Student", "Student@mail.mcgill.ca")
+var Kayla = new Person("Kayla", "kayla@mail.com", 22)
+var Dave = new Person("Dave", "dave@mail.com", 25)
+var Sir = new Person("Sir Edward", "Edward.has.long.email.com", 10)
+var Student = new Person("Student", "Student@mail.mcgill.ca", 3)
 
 var arrayOfObjects = [Kayla, Dave, Sir, Student];
 
@@ -84,10 +85,27 @@ function sortEmail(){
 This function should take a number and return its square. 
 Then, use this function with map on an array of numbers to check the result*/
 
-function squareArray(numToSquare){
+function squareArray(numToSquare) {
     return numToSquare * numToSquare;
 }
 
 var arrayToSquare = [2, 5, 9, 7, 10];
 var squaredArray = arrayToSquare.map(squareArray)
-console.log(squaredArray);
+//console.log(squaredArray);
+
+/*Create a function that can be used with Array.prototype.map. 
+This function should be able to take an object and square its “num” property. 
+Then, use this function with map on an array of objects each containing 
+a “num” property.*/
+/*added a num property to my Person object above */
+
+function squareObj(objToSquare){
+    return objToSquare.num * objToSquare.num;
+}
+
+
+var squaredObjArray = arrayOfObjects.map(squareObj);
+
+console.log(squareObj(arrayOfObjects[0]));
+console.log(squaredObjArray);
+
